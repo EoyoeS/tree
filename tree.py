193dbc21da8aa2c21a45cd70b0a7e643cc2cc547
EoyoeS <2271021509@qq.com>
2023-01-11 20:45:21 +0800

    Output to a file

--- tree.py
+++ tree.py
@@ -1,6 +1,7 @@
 import argparse
 import pathlib
 from collections import namedtuple
+from typing import Generator
 
 
 Symbols = namedtuple(
@@ -22,7 +23,9 @@ all_symbols = (
 symbols: Symbols
 
 
-def get_rows(path: pathlib.Path, node_str: str, pass_str: str):
+def get_rows(
+    path: pathlib.Path, node_str: str, pass_str: str
+) -> Generator[str, None, None]:
     # 生成用于打印的字符串
     yield node_str + path.name
     # 如果是目录,就递归
@@ -31,7 +34,7 @@ def get_rows(path: pathlib.Path, node_str: str, pass_str: str):
             yield pass_str + sub_file
 
 
-def get_tree_nodes(path: pathlib.Path):
+def get_tree_nodes(path: pathlib.Path) -> Generator[str, None, None]:
     path_iter = path.iterdir()
     try:
         # 这里是为了得到迭代的最后一个（最后一个的显示和其他的不同），延迟生成
@@ -61,11 +64,19 @@ def main():
         "--ascii",
         action="store_true",
         default=False,
-        help="Use ASCII instead of extended characters.",
+        help="Use ASCII instead of extended characters",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        metavar="FILE",
+        type=str,
+        help="Output to a file.",
     )
     args = parser.parse_args()
     path = args.path
     use_ascii = args.ascii
+    file_str = args.file
     global symbols
     symbols = all_symbols[use_ascii]
 
@@ -77,9 +88,15 @@ def main():
         if not path.is_dir():
             raise NotADirectoryError("the directory does NOT exist")
     # 开始输出
-    print(path)
-    for text in get_tree_nodes(path):
-        print(text)
+    if file_str is not None:
+        with open(file_str, "x", newline="\n") as f:
+            f.write(f"{path}\n")
+            for text in get_tree_nodes(path):
+                f.write(f"{text}\n")
+    else:
+        print(path)
+        for text in get_tree_nodes(path):
+            print(text)
 
 
 if __name__ == '__main__':
